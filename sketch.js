91a2c1607db9e7f4458293816997300479c8df6e
Scott Render <64731759+scottsss123@users.noreply.github.com>
2025-01-24 10:19:19 +0000

    Added quality of dev features, mars and mercury
    keybord c to log camera position & zoom
    keyboard p to prompt, type body name, pans camera to body 
    click on body to log body name

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 // x / 1.3914e9 maps diameter of sun to diameter of x pixels
 const sf = 100 / 1.3914e9
-let camSpeed = 3e8;
+let camSpeed = 1.496e11 / 300;
 let camVel = [0,0];
 let screenWidth = 0;
 let screenHeight = 0;
 
 
-class Body {
+class Body { // TODO: ideal zoom attribute for when panning straight to body it fits frame nicely
 	#name;
 	#mass; //kg
 	#diameter; //m
@@ -28,6 +28,9 @@ class Body {
 	}
 
 	// getters
+	getName() {
+		return this.#name;
+	}
 	getDiameter() {
 		return this.#diameter;
 	}
@@ -52,9 +55,29 @@ class Camera {
 		this.#zoom *= scale;
 	}
 
-	updatePos(vec2d) {
-		this.#pos[0] += vec2d[0];
-		this.#pos[1] += vec2d[1];
+	updatePos(displacement) {
+		this.#pos[0] += displacement[0] / this.#zoom;
+		this.#pos[1] += displacement[1] / this.#zoom;
+	}
+
+	setPos(pos) {
+		this.#pos[0] = pos[0];
+		this.#pos[1] = pos[1];
+	}
+
+	getAdjustedPos(absolute_pos) {
+		return [((absolute_pos[0] - this.#pos[0]) * sf * this.#zoom) + (screenWidth/2), ((absolute_pos[1] - this.#pos[1]) * sf * this.#zoom) + (screenHeight/2)]
+	}
+
+	getAdjustedDiameter(absolute_diameter) {
+		return this.#zoom * sf * absolute_diameter
+	}
+
+	getZoom() {
+		return this.#zoom;
+	}
+	getPos() {
+		return this.#pos;
 	}
 
 	display(body) {
@@ -63,15 +86,17 @@ class Camera {
 		noStroke();
 		
 		let absolute_pos = body.getPos();
-		let adjusted_pos = [((absolute_pos[0] - this.#pos[0]) * sf * this.#zoom) + (screenWidth/2), ((absolute_pos[1] - this.#pos[1]) * sf * this.#zoom) + (screenHeight/2)];
-		let adjusted_diameter = this.#zoom * sf * body.getDiameter();
+		let adjusted_pos = this.getAdjustedPos(absolute_pos);
+		let adjusted_diameter = this.getAdjustedDiameter(body.getDiameter());
 		ellipse(adjusted_pos[0], adjusted_pos[1], adjusted_diameter);
 	}
 }
 
+let bodies = [];
 let sun;
 let earth;
 let moon;
+let mars;
 let camera;
 
 function setup() {
@@ -79,9 +104,13 @@ function setup() {
 	displayMode('centered');	
 
 	camera = new Camera([0,0],1);
-	sun = new Body("sun", 1.9885e30, 1.3914e9, 0, 0, 'yellow');
-	earth = new Body("earth", 5.972e24, 1.275627e7, 1.496e11, 0, 'blue');
-	moon = new Body("moon", 7.35e10^22, 3.5e6, 1.496e11, -8.8417e7, 'grey');
+	
+	bodies.push(new Body("sun", 1.9885e30, 1.3914e9, 0, 0, 'yellow'));
+	bodies.push(new Body("mercury", 3.3011e23, 4.88e6, 5.791e10, 0, 'grey'))
+	bodies.push(new Body("earth", 5.972e24, 1.275627e7, 1.496e11, 0, 'blue'));
+	bodies.push(new Body("moon", 7.35e22, 3.5e6, 1.496e11, -8.8417e7, 'grey'));
+	bodies.push(new Body("mars", 6.4191e23, 6.79238e6, 2.2794e11, 0, 'red'));
+	
 }//1.275627e7
 
 function draw() {
@@ -90,9 +119,9 @@ function draw() {
 
 	background('black');
 
-	camera.display(sun);
-	camera.display(earth);
-	camera.display(moon);
+	for (let body of bodies) {
+		camera.display(body);
+	}
 
 	if (kb.pressing('d')) { 
 		camVel = [camSpeed,0];
@@ -116,6 +145,20 @@ function draw() {
 		camera.alterZoom(1/1.1);
 	}
 
+	if (kb.presses('c')) {
+		console.log(camera.getPos());
+		console.log(camera.getZoom());
+	}
+
+	if (kb.presses('p')) {
+		let p = prompt("enter body name:");
+		for (let body of bodies) {
+			if (body.getName() === p) {
+				camera.setPos(body.getPos());
+			}
+		}
+	}
+
 	camera.updatePos(camVel);
 }
 
@@ -124,5 +167,17 @@ function mouseWheel(event) {
 		camera.alterZoom(1/1.1);
 	} else { // up
 		camera.alterZoom(1.1);
+	}
+}
+
+function mousePressed() {
+	for (let body of bodies) {
+		
+		let bodyScreenCentre = camera.getAdjustedPos(body.getPos());
+		let bodyScreenRadius = 0.5 * camera.getAdjustedDiameter(body.getDiameter());
+
+		if (dist(mouseX, mouseY, bodyScreenCentre[0], bodyScreenCentre[1]) <= bodyScreenRadius) {
+			console.log(body.getName());
+		}
 	}
 }
